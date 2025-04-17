ed6e9dd0a3ce57c036bd40997d8f8ee6e75f900a
Nicao-ps <nps.guimaraes@gmail.com>
2025-04-17 20:42:59 -0300

    testa mudança

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,4 +1,4 @@
-nome = input('Informe o nome do aluno: ')
-nota = float(input('Informe a nota final do aluno: '))
-
-print(f'O nome do aluno é {nome}, e sua nota final foi {nota:.2f}.')
+for n in range(2):
+    nome = input('Informe o nome do aluno: ')
+    nota = float(input('Informe a nota final do aluno: '))
+    print(f'O nome do aluno é {nome}, e sua nota final foi {nota:.2f}.')
